f8ddd98eb70d652eb1d32533b99ff90bafddf7fd
waihap.chan93 <waihap.chan@titansoft.com.sg>
2021-02-07 18:26:16 +0800

    update comments in Q234 solution

--- leetcode/Q234_PalindromeLinkedList/Solution.java
+++ leetcode/Q234_PalindromeLinkedList/Solution.java
@@ -9,6 +9,42 @@ class ListNode {
 }
 
 class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        // Get the middle point of the list
+        // e.g. if there are 3 elements with indice [0..2], mid will be 1, curr will be 3 (null)
+        // e.g. if there are 4 elements with indice [0..3], mid will be 1, curr will be 3 (not-null)
+        ListNode mid = head, curr = head.next;
+        while (curr != null && curr.next != null) {
+            mid = mid.next;
+            curr = curr.next.next;
+        }
+
+        // End of first half / Mid point -> Start of second half
+        mid = mid.next;
+
+        // Reverse second half linked list in-place
+        ListNode secondHalf = null;
+        while (mid != null) {
+            curr = mid;
+            mid = mid.next;
+            curr.next = secondHalf;
+            secondHalf = curr;
+        }
+        
+        // Compare first half & second half of the linked list
+        curr = head;
+        while (secondHalf != null) {
+            if (curr.val != secondHalf.val) {
+                return false;
+            }
+            curr = curr.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+    
     public static void main(String[] args) {
         int[] input = new int[]{ 1, 0, 1 };
         ListNode root = new ListNode();
@@ -21,34 +57,5 @@ class Solution {
 
         Solution solution = new Solution();
         System.out.println(solution.isPalindrome(root));
-    }
-
-    public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) return true;
-
-        ListNode mid = head, curr = head.next;
-        while (curr != null && curr.next != null) {
-            mid = mid.next;
-            curr = curr.next.next;
-        }
-        mid = mid.next;
-
-        ListNode secondHalf = null;
-        while (mid != null) {
-            curr = mid;
-            mid = mid.next;
-            curr.next = secondHalf;
-            secondHalf = curr;
-        }
-        
-        curr = head;
-        while (secondHalf != null) {
-            if (curr.val != secondHalf.val) {
-                return false;
-            }
-            curr = curr.next;
-            secondHalf = secondHalf.next;
-        }
-        return true;
     }
 }
