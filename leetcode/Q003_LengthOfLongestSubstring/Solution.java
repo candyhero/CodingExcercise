baf087c151706760a8d749949cf5686af67a8455
waihap.chan93 <waihap.chan@titansoft.com.sg>
2021-02-08 23:50:37 +0800

    add Q003 solution

--- leetcode/Q003_LengthOfLongestSubstring/Solution.java
+++ leetcode/Q003_LengthOfLongestSubstring/Solution.java
@@ -4,37 +4,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        if (s.length() < 2) {
-            return s.length();
-        }
-        
-        Map<Character, Integer> map = new HashMap<>();
-        int lastRepeatedCharIndex = 0, lengthOfLongestSubstring = 1; 
-
-        for (int i = 1; i < s.length(); ++i) {
-            char c = s.charAt(i);
-            
-            // Is current char repeated?
-            // Then currentLength = last length
-            // update index then continue 
-            if(map.containsKey(c)) {
-                lastRepeatedCharIndex = map.get(c);
-                continue;
-            }
-    
-            // Is current char not repeated?
-            // calculate length
-            int currentSubstringLength = i - lastRepeatedCharIndex; 
-            lengthOfLongestSubstring = Math.max(currentSubstringLength, lengthOfLongestSubstring);
-            map.put(c, i);
-        }
-        return lengthOfLongestSubstring;
-    }
-
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int l = solution.lengthOfLongestSubstring("pwwkew");
+        int l = solution.lengthOfLongestSubstring("abba");
         System.out.println(l);
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        // 0 or 1 characters
+        if (s.length() < 2) { return s.length(); }
+        
+        Map<Character, Integer> map = new HashMap<>();
+        int lastRepeatIndex = -1;
+        int max = 1; 
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            System.out.println(String.format("%c, %d, %d", c, lastRepeatIndex, max));
+            if (map.containsKey(c)) {
+                // e.g. 
+                // abcabc, i = 5, c = 'a', lastCharIndex = 2, lastRepeatIndex = 1
+                // abccba, i = 5, c = 'a', lastCharIndex = 0, lastRepeatIndex = 3
+                // abccba, i = 1, c = 'b', lastCharIndex = 0, lastRepeatIndex = 0
+                // aabbcc, i = 5, c = 'c', lastCharIndex = 4, lastRepeatIndex = 3
+                int lastCharIndex = map.get(c);
+                if (lastCharIndex > lastRepeatIndex) {
+                    max = Math.max(max, i - lastCharIndex);
+                }
+                lastRepeatIndex = Math.max(lastCharIndex, lastRepeatIndex);
+            }
+            if (i > lastRepeatIndex) {
+                max = Math.max(max, i - lastRepeatIndex);
+            }
+            map.put(c, i);
+            System.out.println(String.format("%c, %d, %d", c, lastRepeatIndex, max));
+        }
+        return max;
     }
 }
